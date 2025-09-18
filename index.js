96665088569f45c094b4efc7981651bef707caa7
israelcena <israelsjm@gmail.com>
2025-09-18 11:12:37 -0300

    adiciona função para importar dados de serviços e ajusta a lógica de importação de itens

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ async function create(itemTabela) {
   console.log("Vou criar no banco:", itemTabela);
 }
 
-// Função principal
 async function importarCatmat() {
   try {
     // Descobrir total de páginas
@@ -46,13 +45,15 @@ async function importarCatmat() {
             descricao: item.descricaoItem,
             tipo: "Permanente",
             codigo_universal_catmat: String(item.codigoItem),
-            ativo: "S"
+            ativo: item.statusItem ? "S" : "N",
+            atualizacao: item.dataHoraAtualizacao
           };
 
           await create(registroTabela);
         }
       } catch (errPag) {
         console.error("ERRO página " + pagina + ":", errPag.message);
+        continue;
         // Se quiser continuar mesmo com erro: continue;
         // Se quiser parar: throw errPag;
       }
@@ -63,4 +64,57 @@ async function importarCatmat() {
   }
 }
 
-importarCatmat();
+async function importarCatser() {
+  try {
+    // Descobre total de páginas
+    const primeiraUrl = `${SERVICE_URL}?tamanhoPagina=${TAMANHO_PAGINA}&pagina=1`;
+    const primeiraResp = await axios.get(primeiraUrl);
+    const totalPaginas = primeiraResp.data.totalPaginas;
+    console.log("TOTAL DE PÁGINAS:", totalPaginas);
+
+    // Para cada página...
+    for (let pagina = 1; pagina <= totalPaginas; pagina++) {
+      const url = `${SERVICE_URL}?tamanhoPagina=${TAMANHO_PAGINA}&pagina=${pagina}`;
+      console.log(`Baixando página ${pagina}/${totalPaginas}`);
+
+      try {
+        const resp = await axios.get(url);
+        const itens = resp.data.resultado;
+
+        for (const item of itens) {
+          // Monta objeto conforme tabela
+          const registroTabela = {
+            idcatmat: item.codigoServico,
+            nome: item.nomeServico,
+            descricao: item.nomeClasse,
+            tipo: "Serviço",
+            codigo_universal_catmat: String(item.codigoItem),
+            ativo: item.statusServico ? "S" : "N",
+            atualizacao: item.dataHoraAtualizacao
+          };
+
+          await create(registroTabela);
+        }
+      } catch (errPag) {
+        console.error("ERRO página " + pagina + ":", errPag.message);
+        continue; // Comente ou descomente conforme desejar pular a página com erro
+      }
+    }
+    console.log("-- FINALIZADO [SERVIÇOS] --");
+  } catch (err) {
+    console.error("Erro inicial:", err.message);
+  }
+}
+
+// --- Função para rodar ambas paralelamente
+async function importarCatmatECatserParalelo() {
+  await Promise.all([
+    importarCatmat(),
+    importarCatser()
+  ]);
+  console.log("AMBOS PROCESSOS FINALIZADOS");
+}
+
+// Para executar:
+importarCatmatECatserParalelo();
+
